2f4f81cf838a77b2e4d4805d6460044c1aa5e746
cessbub <cess.ventures209@gmail.com>
2024-06-13 08:19:52 +0800

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import os
 import sys
+
+# Add the app directory to the system path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
-import streamlit.web.cli as stcli
 
 if __name__ == "__main__":
-    import streamlit.cli as stcli
+    from streamlit.web import cli as stcli
     sys.argv = ["streamlit", "run", "app/run.py"]
     sys.exit(stcli.main())
