e70050ccc802bf177d3e38252d80e3a572c5354f
cessbub <cess.ventures209@gmail.com>
2024-06-13 08:05:35 +0800

    Edited main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
 
 import streamlit as st
 from app.auth import authenticator
