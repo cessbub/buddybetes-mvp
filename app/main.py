3da853f4c30c6c91b2b7109d8e19a303febd76e5
cessbub <cess.ventures209@gmail.com>
2024-06-13 08:17:05 +0800

    Modified main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
+import streamlit.web.cli as stcli
 
 if __name__ == "__main__":
     import streamlit.cli as stcli
